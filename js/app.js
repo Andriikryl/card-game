f4e9ee51709b3d4e756130a8c747259237b434f4
Andriikryl <andreyk557@gmail.com>
2023-04-03 15:29:11 +0300

    feat[create-card-func]

--- js/app.js
+++ js/app.js
@@ -4,3 +4,28 @@ const cardObjectDefinitions = [
   { id: 3, imagePath: "/images/card-QueenDiamonds.png" },
   { id: 4, imagePath: "/images/card-AceSpades.png" },
 ];
+
+/* <div class="card">
+<div class="card-inner">
+    <div class="card-front">
+        <img src="/images/card-JackClubs.png" alt="" class="card-img">
+    </div>
+    <div class="card-back">
+        <img src="/images/card-back-Blue.png" alt="" class="card-img">
+    </div>
+</div>
+</div> */
+
+function createCard(cardItem) {
+  const carfElem = document.createElement("div");
+  const cardInnerElem = createElement("div");
+  const cardFrontElem = createElement("div");
+  const cardBackElem = createElement("div");
+
+  const cardFrontImg = createElement("img");
+  const cardBackImg = createElement("img");
+}
+
+function createElement(elemType) {
+  return document.createElement(elemType);
+}
